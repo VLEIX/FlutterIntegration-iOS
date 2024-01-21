b427190b745e1d1591bb5e92f08dc41db4b5909a
VLEIX <vleix.92@gmail.com>
2024-01-21 12:22:57 -0500

    Initial Commit

---
+++ FlutterIntegration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlutterIntegration
+//
+//  Created by Flavio Franco on 21/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlutterIntegration/FlutterIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlutterIntegrationApp.swift
+//  FlutterIntegration
+//
+//  Created by Flavio Franco on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FlutterIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
